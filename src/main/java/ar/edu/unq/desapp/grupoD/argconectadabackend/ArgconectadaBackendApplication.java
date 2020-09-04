51d05f73944a68843a60ba4760a8c8aa2582a720
Luis Coronel <lucho_9292@live.com>
2020-09-04 13:55:22 -0300

    :octocat: created base project with initializr, better gitignore

---
+++ src/main/java/ar/edu/unq/desapp/grupoD/argconectadabackend/ArgconectadaBackendApplication.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoD.argconectadabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArgconectadaBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArgconectadaBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ar/edu/unq/desapp/grupoD/argconectadabackend/ArgconectadaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ar.edu.unq.desapp.grupoD.argconectadabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArgconectadaBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
