edd73ec05dc9e49e10d6b1558d08b75676debdfc
Luis Coronel <lucho_9292@live.com>
2020-09-05 18:07:21 -0300

    exclude file to correctly run main class

--- src/main/java/ar/edu/unq/desapp/grupoD/argconectadabackend/ArgconectadaBackendApplication.java
+++ src/main/java/ar/edu/unq/desapp/grupoD/argconectadabackend/ArgconectadaBackendApplication.java
@@ -3,7 +3,7 @@ package ar.edu.unq.desapp.grupoD.argconectadabackend;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration.class})
 public class ArgconectadaBackendApplication {
 
 	public static void main(String[] args) {
