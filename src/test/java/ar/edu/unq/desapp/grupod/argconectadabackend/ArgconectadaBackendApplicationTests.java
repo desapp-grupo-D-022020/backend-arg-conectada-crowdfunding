232d7355154545b65accbfc82603a6717dd495ea
Luis Coronel <lucho_9292@live.com>
2020-09-06 03:43:33 -0300

    better coverage for tests, separated scenarios

--- src/test/java/ar/edu/unq/desapp/grupod/argconectadabackend/ArgconectadaBackendApplicationTests.java
+++ src/test/java/ar/edu/unq/desapp/grupod/argconectadabackend/ArgconectadaBackendApplicationTests.java
@@ -1,13 +1,31 @@
 package ar.edu.unq.desapp.grupod.argconectadabackend;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-@SpringBootTest
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.boot.web.server.LocalServerPort;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
 class ArgconectadaBackendApplicationTests {
+	
+	@LocalServerPort
+	private int port;
+	
+	@Autowired
+	private TestRestTemplate restTemplate;
 
 	@Test
 	void contextLoads() {
+	}
+	
+	@Test
+	void getHomepageWithRandomPortShouldReturnMessage() throws Exception {
+    	String response = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
+		assertTrue(response.contentEquals("Hello, World!"), "wrong response");
 	}
 
 }
--- src/test/java/ar/edu/unq/desapp/grupod/argconectadabackend/HomeTest.java
+++ src/test/java/ar/edu/unq/desapp/grupod/argconectadabackend/HomeTest.java
@@ -6,21 +6,12 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
-import org.springframework.boot.test.web.client.TestRestTemplate;
-import org.springframework.boot.web.server.LocalServerPort;
 
-@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class HomeTest {
-	
-	@LocalServerPort
-	private int port;
 	
 	@Autowired
 	private HomeController controller;
-	
-	@Autowired
-	private TestRestTemplate restTemplate;
 
     @Test
     void contextLoads() throws Exception {
@@ -28,8 +19,8 @@ class HomeTest {
     }
     
     @Test
-	void homeShouldReturnDefaultMessage() throws Exception {
-    	String response = this.restTemplate.getForObject("http://localhost:" + port + "/", String.class);
-		assertTrue(response.contentEquals("Hello, World!"), "wrong response");
-	}
+    void homeShouldReturnMessage() {
+    	String response = controller.welcome();
+    	assertTrue(response.contentEquals("Hello, World!"), "Wrong response");
+    }
 }
