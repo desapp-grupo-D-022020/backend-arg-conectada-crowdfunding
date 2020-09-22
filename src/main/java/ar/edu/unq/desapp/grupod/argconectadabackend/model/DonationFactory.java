9bde7b9a8f3cd193c03ef40509b094f0b997db52
Luis Coronel <lucho_9292@live.com>
2020-09-22 18:22:10 -0300

    fix DonationFactory uses a new user for each method

--- src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/model/DonationFactory.java
+++ src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/model/DonationFactory.java
@@ -7,35 +7,40 @@ public class DonationFactory {
 	
 	public static Donation anyDonation() {
 		LocalDateTime dateTime = LocalDateTime.now();
-		Donation anyDonation = new Donation("aNickname", 1000, dateTime,"aCommentary");
+		Donor anyDonor = DonorFactory.anyDonor();
+		Donation anyDonation = new Donation(anyDonor, 1000, dateTime,"aCommentary");
 		
 		return anyDonation;
 	}
 	
 	public static Donation firstDayOfMonthDonation() {
 		LocalDateTime dateTime = LocalDateTime.now().with(TemporalAdjusters.firstDayOfMonth());
-		Donation firstDayOfMonthDonation = new Donation("aNickname", 1000, dateTime,"aCommentary");
+		Donor anyDonor = DonorFactory.anyDonor();
+		Donation firstDayOfMonthDonation = new Donation(anyDonor, 1000, dateTime,"aCommentary");
 		
 		return firstDayOfMonthDonation;
 	}
 	
 	public static Donation lastDayOfMonthDonation() {
 		LocalDateTime dateTime = LocalDateTime.now().with(TemporalAdjusters.lastDayOfMonth());
-		Donation lastDayOfMonthDonation = new Donation("aNickname", 1000, dateTime,"aCommentary");
+		Donor anyDonor = DonorFactory.anyDonor();
+		Donation lastDayOfMonthDonation = new Donation(anyDonor, 1000, dateTime,"aCommentary");
 		
 		return lastDayOfMonthDonation;
 	}
 	
 	public static Donation beforeMonthDonation() {
 		LocalDateTime dateTime = LocalDateTime.now().with(TemporalAdjusters.firstDayOfMonth()).minusDays(1);
-		Donation beforeMonthDonation = new Donation("aNickname", 1000, dateTime,"aCommentary");
+		Donor anyDonor = DonorFactory.anyDonor();
+		Donation beforeMonthDonation = new Donation(anyDonor, 1000, dateTime,"aCommentary");
 		
 		return beforeMonthDonation;
 	}
 	
 	public static Donation afterMonthDonation() {
 		LocalDateTime dateTime = LocalDateTime.now().with(TemporalAdjusters.lastDayOfMonth()).plusDays(1);
-		Donation afterMonthDonation = new Donation("aNickname", 1000, dateTime,"aCommentary");
+		Donor anyDonor = DonorFactory.anyDonor();
+		Donation afterMonthDonation = new Donation(anyDonor, 1000, dateTime,"aCommentary");
 		
 		return afterMonthDonation;
 	}
