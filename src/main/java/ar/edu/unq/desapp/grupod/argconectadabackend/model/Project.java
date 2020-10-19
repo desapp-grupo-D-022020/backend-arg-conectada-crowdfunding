70e8afc6db9aa40f3f2d18f33d2d7ef8951a3351
CristianAranguren <c.knoxville8@gmail.com>
2020-10-19 18:46:41 -0300

    agregada relacion con place en project
    Signed-off-by: CristianAranguren <c.knoxville8@gmail.com>

--- src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/model/Project.java
+++ src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/model/Project.java
@@ -14,6 +14,7 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Transient;
 
 @Entity
@@ -23,8 +24,8 @@ public class Project {
 	private int id;
 	@Column
 	private String name;
-	//@Column
-	@Transient
+	@OneToOne(cascade=CascadeType.ALL) 	
+	@JoinColumn(name="place_id")
 	private Place place;
 	@Column
 	private double factor;
