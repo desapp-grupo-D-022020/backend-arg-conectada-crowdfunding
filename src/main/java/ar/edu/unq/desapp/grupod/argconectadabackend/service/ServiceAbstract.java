05655326c17df16ef666872471bf940724115d59
CristianAranguren <c.knoxville8@gmail.com>
2020-10-22 20:02:45 -0300

    agregada transaccionalidad a los servicios
    Signed-off-by: CristianAranguren <c.knoxville8@gmail.com>

--- src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/service/ServiceAbstract.java
+++ src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/service/ServiceAbstract.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public abstract class ServiceAbstract<T, ID> implements IService<T, ID> {
@@ -15,19 +16,24 @@ public abstract class ServiceAbstract<T, ID> implements IService<T, ID> {
 	public ServiceAbstract(JpaRepository<T, ID> repo) {
 		this.repo = repo;
 	}
-
+	
+	@Transactional
 	@Override
 	public List<T> getAll() { return (List<T>) repo.findAll(); }
 	
+	@Transactional
 	@Override
 	public T getById(ID id) { return repo.getOne(id); }
 
+	@Transactional
 	@Override
 	public void save(T t) { repo.save(t); }
 
+	@Transactional
 	@Override
 	public void update(T t) { repo.save(t); }
 	
+	@Transactional
 	@Override
 	public void delete(ID id) { repo.deleteById(id); } 
 }
