b93f52639701315580f002ee275fc069f8d23b6f
Luis Coronel <lucho_9292@live.com>
2020-10-27 11:03:08 -0300

    swagger api info, empty beans dont fail (for api testing)

--- src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/SwaggerConfig.java
+++ src/main/java/ar/edu/unq/desapp/grupod/argconectadabackend/SwaggerConfig.java
@@ -1,11 +1,16 @@
 package ar.edu.unq.desapp.grupod.argconectadabackend;
 
+import java.util.Collections;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.data.rest.configuration.SpringDataRestConfiguration;
 import springfox.documentation.spring.web.plugins.Docket;
@@ -21,6 +26,15 @@ public class SwaggerConfig {
           .select()                                  
           .apis(RequestHandlerSelectors.any())              
           .paths(PathSelectors.any())                          
-          .build();                                           
+          .build()
+          .apiInfo(apiInfo());
+    }
+    
+    private ApiInfo apiInfo() {
+    	ApiInfo apiInfo = new ApiInfo(
+    			"Argentina Conectada REST API", "API Documentation.", "v1.0", "",
+    			new Contact("DEV Environment", "https://frontend-arg-conectada-develop.herokuapp.com/", ""),
+    			"License of API", "https://www.apache.org/licenses/LICENSE-2.0", Collections.emptyList());
+        return apiInfo;
     }
 }
